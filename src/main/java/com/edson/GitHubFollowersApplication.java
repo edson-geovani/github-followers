04621c75da377d3f775bba6de6ae9b41add8249b
Edson Nascimento <e.geovani.nascimento@gmail.com>
2019-10-10 21:15:00 -0400

    initial commit

---
+++ src/main/java/com/edson/GitHubFollowersApplication.java
@@ -0,0 +1,13 @@
+package com.edson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubFollowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubFollowersApplication.class, args);
+	}
+
+}
